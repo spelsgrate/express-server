3a7209c02e11c714732345ac1ce45ad8800fb0d2
EC2 Default User <ec2-user@ip-172-31-80-220.ec2.internal>
2020-04-29 23:05:53 +0000

    add pages and handles

--- server.js
+++ server.js
@@ -1,40 +1,52 @@
 const express = require('express')
+const expressHandlebars = require('express-handlebars')
 
 const app = express()
+const handlers = require('./lib/handlers')
+// configure Handlebars view engine
+app.engine('handlebars', expressHandlebars({
+  defaultLayout: 'main',
+}))
+app.set('view engine', 'handlebars')
 
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.type('text/plain')
-  res.send('Meadowlark Travel');
+app.use(express.static(__dirname + '/public'))
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.get('/', (req, res) => res.render('home'))
+
+const fortunes = [
+  "Honesty is the first chapter in the book of wisdom.",
+  "There is only one corner of the universe you can be certain of improving, and that's your own self.",
+  "I have not failed. I've just found 10,000 ways that won't work.",
+  "The secret of getting ahead is getting started.",
+  "It is during our darkest moments that we must focus to see the light.",
+]
+
+app.get('/quotes', (req, res) => {
+  const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
+  res.render('quotes', { fortune: randomFortune })
 })
 
-app.get('/about', (req, res) => {
-  res.type('text/plain')
-  res.send('About Meadowlark Travel')
-})
+app.get('/newsletter-signup', handlers.newsletterSignup)
+app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
+app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
 
 // custom 404 page
 app.use((req, res) => {
-  res.type('text/plain')
   res.status(404)
-  res.send('404 - Not Found')
-})
-
-
-// custom 404 page
-app.use((req, res) => {
-  res.type('text/plain')
-  res.status(404)
-  res.send('404 - Not Found')
+  res.render('404')
 })
 
 // custom 500 page
 app.use((err, req, res, next) => {
   console.error(err.message)
-  res.type('text/plain')
   res.status(500)
-  res.send('500 - Server Error')
+  res.render('500')
 })
 
 app.listen(port, () => console.log(
